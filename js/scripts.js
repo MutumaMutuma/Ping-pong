26d993bd75e7e87e5193de6fd6dec476bdc4e9dd
MutumaMutuma <lewismutuma1000@gmail.com>
2018-07-13 19:47:25 +0300

    added a for

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
-alert("Hi there!")
+alert("Hi there")
 //business logic
 var numbersToBePingponged = [];
+
+function pingPong (number){
+
+});
+
+
+//user interface logic
+
+$(document).ready(function(){
+  
+});
